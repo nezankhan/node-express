31bab145371501a82593642ae727aa1414bea35c
nezankhan <nizankhan@gmail.com>
2023-02-24 19:23:11 -0800

    Workshop1Complete

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-const campsiteRouter = require('./routes/campsiteRouter')
-const promotionRouter = require('./routes/promotionRouter')
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/campsites',campsiteRouter)
-app.use('/promotions',promotionRouter)
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 
 app.use(express.static(__dirname + '/public'));
